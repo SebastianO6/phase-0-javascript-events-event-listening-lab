18839aa7bc74ac686dfb7b3d394d1421b5c41287
Bill <bill.omondi@student.moringaschool.com>
2025-03-21 15:19:56 +0300

    events

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+
 function addingEventListener() {
+    const input = document.getElementById('button');
+    input.addEventListener('click' , addingEventListener)
+    alert('I have been clicked!')
 }
+
+
+
+
+// addingEventListener(); // Call the function to ensure it binds the event
